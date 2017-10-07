cc4f246dbaaba4b219b90908356bcf3c88685e5e
boomkin <bence.halpern14@imperial.ac.uk>
2017-10-07 15:49:28 +0100

    Code used for second submission

--- titanic/mainTitanic.py
+++ titanic/mainTitanic.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,30 +8,39 @@ from sklearn.model_selection import train_test_split
 
 # Imputing the age 
 def age_imputing(df): 
-    # You want an age distribution for Ms and Mrs
-    df.Age[df.Name.str.contains("Miss")].plot(kind='kde')    
-    df.Age[df.Name.str.contains("Mrs")].plot(kind='kde')    
-    # plots an axis lable
-    plt.xlabel("Age")    
-    plt.title("Age Distribution within Mr and Miss")
-    # sets our legend for our graph.
-    plt.legend(('1st Class', '2nd Class','3rd Class'),loc='best') 
-    plt.show()
+    # Impute the means of Mr and Mrs to missing data
+    missMean = df.Age[df.Name.str.contains("Miss")].mean()
+    mrsMean = df.Age[df.Name.str.contains("Mrs")].mean()
+    
+    df.Age[(df.Name.str.contains("Miss")) & (df['Age'].isnull())] = missMean
+    df.Age[(df.Name.str.contains("Mrs")) & (df['Age'].isnull())] = mrsMean
+    return df
 
-def normal_cleaning(df):
+def normal_cleaning(df, cv):
+    # Age Imputing
+    before = df.Age.isnull().sum()
+    df = age_imputing(df)
+    after = df.Age.isnull().sum()
+    print("Age imputing solves this many entries:" + str(before-after))
+    
     dropped = df.fillna(0)
+    
+    # Binary encoding of gender
+    sex = {'male':1, 'female':0}
+    dropped['Sex'] = dropped['Sex'].map(sex)
+    print(dropped)
     # Keep age and fare for now
-    dropped = dropped[["PassengerId","Age", "Fare", "Pclass", "Survived","Parch"]]
-    return dropped
-def normal_cleaningCV(df):
-    dropped = df.fillna(0)
-    # Keep age and fare for now
-    dropped = dropped[["PassengerId","Age", "Fare", "Pclass","Parch"]]
+    if (cv):
+            dropped = dropped[["PassengerId","Age", "Fare", "Pclass","Parch", "Sex"]]
+    else:
+        dropped = dropped[["PassengerId","Age", "Fare", "Pclass", "Survived","Parch", "Sex"]]
     return dropped
 
 def random_forest_model(training_data, validation_data, testing_data):
     cols_to_delete = ["Survived", "PassengerId"]
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf = RandomForestClassifier(max_depth=None,\
+                                 random_state=0,\
+                                  n_estimators=100)
     clf.fit(training_data.drop(cols_to_delete, axis=1), \
              training_data["Survived"])
     predicted = clf.predict(training_data.drop(cols_to_delete, axis=1))
@@ -48,6 +56,8 @@ def random_forest_model(training_data, validation_data, testing_data):
             axis=1))
     output_table = output_table.drop("Age", axis=1)
     return output_table
+
+
     
 
 
@@ -60,14 +70,13 @@ training_data, validation_data = train_test_split(input_data, test_size=0.2)
 #ageImputing(df)
 
 
-cleaned_training_data = normal_cleaning(training_data)
-cleaned_validation_data = normal_cleaning(validation_data)
-cleaned_testing_data = normal_cleaningCV(testing_data)
+cleaned_training_data = normal_cleaning(training_data, False)
+cleaned_validation_data = normal_cleaning(validation_data, False)
+cleaned_testing_data = normal_cleaning(testing_data, True)
 
 result = random_forest_model(cleaned_training_data, cleaned_validation_data, \
         cleaned_testing_data)
 
-print(result)
 
 result.to_csv("result.csv", index=False)
 
